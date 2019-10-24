299f4ed7022b02a916c8fbce8bec7d3676a428fa
Abdur Rohman <abdur.rohman2883@gmail.com>
2019-10-24 16:55:16 +0700

    SQLite database

--- app/src/main/java/com/biceps_studio/task_layout/adapter/PostAdapter.kt
+++ app/src/main/java/com/biceps_studio/task_layout/adapter/PostAdapter.kt
@@ -1,11 +1,13 @@
 package com.biceps_studio.task_layout.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.biceps_studio.task_layout.R
 import com.biceps_studio.task_layout.data.PostModel
+import com.biceps_studio.task_layout.utils.Database
 import kotlinx.android.synthetic.main.item_post.view.*
 
 class PostAdapter : RecyclerView.Adapter<PostAdapter.ViewHolder>() {
@@ -42,6 +44,28 @@ class PostAdapter : RecyclerView.Adapter<PostAdapter.ViewHolder>() {
 
         holder.itemView.btnDelete.setOnClickListener { onListener.onDeletePost(position) }
         holder.itemView.cvPost.setOnClickListener { onListener.onClickPost(postModel.id) }
+
+        val context: Context = holder.itemView.context
+
+        val database = Database(context)
+
+        if (database.isExist(postModel.id)){
+            holder.itemView.ivSaved.setImageDrawable(context.getDrawable(R.drawable.ic_star))
+        } else {
+            holder.itemView.ivSaved.setImageDrawable(context.getDrawable(R.drawable.ic_unstar))
+        }
+
+        holder.itemView.ivSaved.setOnClickListener {
+            if (database.isExist(postModel.id)){
+                database.deletePost(postModel.id)
+
+                holder.itemView.ivSaved.setImageDrawable(context.getDrawable(R.drawable.ic_unstar))
+            } else {
+                database.insertPost(postModel)
+
+                holder.itemView.ivSaved.setImageDrawable(context.getDrawable(R.drawable.ic_star))
+            }
+        }
     }
 
     interface OnListener {
