f4576f1e58d5ca02ca3d7486807ba09a5c7c0af1
Abdur Rohman <abdur.rohman2883@gmail.com>
2019-10-24 15:34:10 +0700

    Setup Retrofit

--- app/src/main/java/com/biceps_studio/task_layout/activity/DetailPostActivity.kt
+++ app/src/main/java/com/biceps_studio/task_layout/activity/DetailPostActivity.kt
@@ -28,32 +28,16 @@ class DetailPostActivity : AppCompatActivity() {
 
     private fun initEvent() {
         btnUpdate.setOnClickListener { updatePost(postModel!!) }
+        srlDetail.setOnRefreshListener { initData() }
     }
 
     private fun updatePost(postModel: PostModel) {
         postModel.title = etTitle.text.toString()
 
-        API.updatePost(postModel, object : Callback<PostModel> {
-            override fun onFailure(call: Call<PostModel>, t: Throwable) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-
-            override fun onResponse(call: Call<PostModel>, response: Response<PostModel>) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-
-        })
-    }
-
-    private fun initData() {
-        srlDetail.isRefreshing = true
-
-        if (id == null || id == -1) {
-            intent.getIntExtra(Utils.ID_POST, -1)
-        }
-
         if (Utils.isNetworkConected(getActivity())){
-            API.getPost(id!!, object : Callback<PostModel> {
+            srlDetail.isRefreshing = true
+
+            API.updatePost(postModel, object : Callback<PostModel> {
                 override fun onFailure(call: Call<PostModel>, t: Throwable) {
                     srlDetail.isRefreshing = false //Menghilangkan Loading
 
@@ -65,18 +49,52 @@ class DetailPostActivity : AppCompatActivity() {
                 override fun onResponse(call: Call<PostModel>, response: Response<PostModel>) {
                     srlDetail.isRefreshing = false //Menghilangkan Loading
 
-                    if (response.isSuccessful) {
-                        postModel = response.body()
-
-                        etTitle.setText(postModel!!.title)
-                        tvContent.text = postModel!!.body
+                    if (response.isSuccessful){
+                        Utils.showToast(getActivity(), "Postingan berhasil diubah")
                     } else {
-                        if (BuildConfig.DEBUG){ //Kondisi dalam keadaan mode DEBUG
-                            Utils.showToast(getActivity(), response.message())
-                        }
+                        Utils.showToast(getActivity(), response.message())
                     }
                 }
             })
+        } else {
+            Utils.errorNoConnection(getActivity())
+        }
+    }
+
+    private fun initData() {
+        srlDetail.isRefreshing = true
+
+        if (id == null || id == -1) {
+            id = intent.getIntExtra(Utils.ID_POST, -1)
+        }
+
+        if (Utils.isNetworkConected(getActivity())){
+            if (id != -1){
+                API.getPost(id!!, object : Callback<PostModel> {
+                    override fun onFailure(call: Call<PostModel>, t: Throwable) {
+                        srlDetail.isRefreshing = false //Menghilangkan Loading
+
+                        if (BuildConfig.DEBUG){ //Kondisi dalam keadaan mode DEBUG
+                            Utils.showToast(getActivity(), t.message!!)
+                        }
+                    }
+
+                    override fun onResponse(call: Call<PostModel>, response: Response<PostModel>) {
+                        srlDetail.isRefreshing = false //Menghilangkan Loading
+
+                        if (response.isSuccessful) {
+                            postModel = response.body()
+
+                            etTitle.setText(postModel!!.title)
+                            tvContent.text = postModel!!.body
+                        } else {
+                            if (BuildConfig.DEBUG){ //Kondisi dalam keadaan mode DEBUG
+                                Utils.showToast(getActivity(), response.message())
+                            }
+                        }
+                    }
+                })
+            }
         } else {
             srlDetail.isRefreshing = false
         }
